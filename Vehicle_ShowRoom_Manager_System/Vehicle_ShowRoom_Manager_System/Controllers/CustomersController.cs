c6a16ff6572e576c64cac266336ed82086d1a3aa
Long192 <75301963+Long192@users.noreply.github.com>
2022-05-02 15:53:23 +0630

    update

---
+++ Vehicle_ShowRoom_Manager_System/Vehicle_ShowRoom_Manager_System/Controllers/CustomersController.cs
@@ -0,0 +1,161 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Vehicle_ShowRoom_Manager_System.Models;
+
+namespace Vehicle_ShowRoom_Manager_System.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private Vehicle_ShowRoom_Manager_System_DataEntities db = new Vehicle_ShowRoom_Manager_System_DataEntities();
+
+        // GET: Customers
+        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+
+            ViewBag.CurrentFilter = searchString;
+            var customers = db.Customer.Include(v => v.Sale);
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                customers = customers.Where(v =>
+                    v.CustomerName.ToLower().Contains(searchString.ToLower()));
+            }
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    customers = customers.OrderByDescending(v => v.CustomerName);
+                    break;
+                default:
+                    customers = customers.OrderByDescending(v => v.CustomerName);
+                    break;
+            }
+
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+            return View(customers.ToPagedList(pageNumber, pageSize));
+
+
+        }
+
+        // GET: Customers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customer.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // GET: Customers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CustomerId,CustomerName,Email,Password,Address,Gender,Status")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Customer.Add(customer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(customer);
+        }
+
+        // GET: Customers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customer.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Customers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CustomerId,CustomerName,Email,Password,Address,Gender,Status")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+
+        // GET: Customers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customer.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Customer customer = db.Customer.Find(id);
+            db.Customer.Remove(customer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Vehicle_ShowRoom_Manager_System/Vehicle_ShowRoom_Manager_System/Controllers/ShowRoomsController.cs
@@ -0,0 +1,182 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Vehicle_ShowRoom_Manager_System.Models;
+
+namespace Vehicle_ShowRoom_Manager_System.Controllers
+{
+    public class ShowRoomsController : Controller
+    {
+        private Vehicle_ShowRoom_Manager_System_DataEntities db = new Vehicle_ShowRoom_Manager_System_DataEntities();
+
+        // GET: ShowRooms
+        [Authorize]
+
+        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+
+            ViewBag.CurrentFilter = searchString;
+            var showRoom = db.ShowRoom.Include(s => s.Admin).Include(s => s.Customer).Include(s => s.Vehicle);
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                showRoom = showRoom.Where(v =>
+                    v.Vehicle.VehicleName.ToLower().Contains(searchString.ToLower()));
+            }
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    showRoom = showRoom.OrderByDescending(v => v.Vehicle.VehicleName);
+                    break;
+                default:
+                    showRoom = showRoom.OrderByDescending(v => v.Vehicle.VehicleName);
+                    break;
+            }
+
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+            return View(showRoom.ToPagedList(pageNumber, pageSize));
+
+
+        }
+
+        // GET: ShowRooms/Details/5
+        [Authorize]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ShowRoom showRoom = db.ShowRoom.Find(id);
+            if (showRoom == null)
+            {
+                return HttpNotFound();
+            }
+            return View(showRoom);
+        }
+        [Authorize]
+        // GET: ShowRooms/Create
+        public ActionResult Create()
+        {
+            ViewBag.AdminId = new SelectList(db.Admin, "AdminId", "AdminName");
+            ViewBag.CustomerId = new SelectList(db.Customer, "CustomerId", "CustomerName");
+            ViewBag.VehicleId = new SelectList(db.Vehicle, "VehicleId", "VehicleName");
+            return View();
+        }
+
+        // POST: ShowRooms/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RoomId,RoomAddress,RoomName,AdminId,CustomerId,VehicleId,Status")] ShowRoom showRoom)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ShowRoom.Add(showRoom);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AdminId = new SelectList(db.Admin, "AdminId", "AdminName", showRoom.AdminId);
+            ViewBag.CustomerId = new SelectList(db.Customer, "CustomerId", "CustomerName", showRoom.CustomerId);
+            ViewBag.VehicleId = new SelectList(db.Vehicle, "VehicleId", "VehicleName", showRoom.VehicleId);
+            return View(showRoom);
+        }
+
+        // GET: ShowRooms/Edit/5
+        [Authorize]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ShowRoom showRoom = db.ShowRoom.Find(id);
+            if (showRoom == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AdminId = new SelectList(db.Admin, "AdminId", "AdminName", showRoom.AdminId);
+            ViewBag.CustomerId = new SelectList(db.Customer, "CustomerId", "CustomerName", showRoom.CustomerId);
+            ViewBag.VehicleId = new SelectList(db.Vehicle, "VehicleId", "VehicleName", showRoom.VehicleId);
+            return View(showRoom);
+        }
+
+        // POST: ShowRooms/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public ActionResult Edit([Bind(Include = "RoomId,RoomAddress,RoomName,AdminId,CustomerId,VehicleId,Status")] ShowRoom showRoom)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(showRoom).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AdminId = new SelectList(db.Admin, "AdminId", "AdminName", showRoom.AdminId);
+            ViewBag.CustomerId = new SelectList(db.Customer, "CustomerId", "CustomerName", showRoom.CustomerId);
+            ViewBag.VehicleId = new SelectList(db.Vehicle, "VehicleId", "VehicleName", showRoom.VehicleId);
+            return View(showRoom);
+        }
+
+        // GET: ShowRooms/Delete/5
+        [Authorize]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ShowRoom showRoom = db.ShowRoom.Find(id);
+            if (showRoom == null)
+            {
+                return HttpNotFound();
+            }
+            return View(showRoom);
+        }
+
+        // POST: ShowRooms/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ShowRoom showRoom = db.ShowRoom.Find(id);
+            db.ShowRoom.Remove(showRoom);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
