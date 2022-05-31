c82de7d7b845d71cfea0b284e8616d1e28e15b15
Long192 <75301963+Long192@users.noreply.github.com>
2022-06-01 02:59:10 +0700

    update

--- Vehicle_ShowRoom_Manager_System/Vehicle_ShowRoom_Manager_System/App_Start/RouteConfig.cs
+++ Vehicle_ShowRoom_Manager_System/Vehicle_ShowRoom_Manager_System/App_Start/RouteConfig.cs
@@ -14,12 +14,6 @@ namespace Vehicle_ShowRoom_Manager_System
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                "Vehicle_ShowRoom_Manager_System",
-                "{controller}/{action}/{id}",
-                new { controller = "Admins", action = "Index", id = UrlParameter.Optional }
-                );
-
-            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
